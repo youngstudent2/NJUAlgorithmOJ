def14af3b169c99ba95b4e9176fd076907ff6029
youngstudent2 <644699280@qq.com>
2020-05-26 01:41:26 +0800

    Update a.cpp

--- 7/a.cpp
+++ 7/a.cpp
@@ -1,91 +1,119 @@
 #include <iostream>
+#include <vector>
+#include <list>
 using namespace std;
 
 #define INF 0xFFFFFFF	//INF表示∞
-inline int max(int a,int b){
-    return a>b?a:b;
+inline int max(int a, int b) {
+    return a > b ? a : b;
 }
 
-
-struct node 
+struct Node
 {
-    int k;//记录当前结点编号 
-    struct node *next;
-}*v;
-void topsort(int n,int *indegree)
-{
-	int i;
-	struct node *p;
-	int *stack=(int*)malloc(sizeof(int)*n),top=-1;
-	for(i=1;i<=n;i++)
-	    if(indegree[i]==0)stack[++top]=i;//入度为0的点入栈 
-    while(top>-1)                  //栈非空时循环 
-    {
-    	p=&v[stack[top--]];        //取出栈顶元素 
-    	printf("%d ",stack[top+1]);
-    	while(p->next!=NULL)       //枚举该点所有后继 
-    	{
-    		p=p->next;
-	    	if(--indegree[p->k]==0)      //若去除一条边后入度为0则入栈 
-	    	    stack[++top]=p->k;		
-	    }    	
-    }	
-}
+    int index;
+    bool vis;
+    int out_d;
+    int dis;
+    list<Edge> edges; 
+    Node() {
+        vis = false;
+        out_d = 0;
+    }
+    void addEdge(int to,int length){
+        edges.push_back(Edge(to,length));
+    }
+};
 
-struct MGraph
-{  	
-	int **edges;		//邻接矩阵
-   	int n;   
-    int *indegree;					
-    int *dp;
-    int *vis;
-    MGraph(int n){
-        this->n=n;
-        edges = new int*[n];
-        for(int i=0;i<n;++i){
-            edges[i] = new int[n];
-            for(int j=0;j<n;++j){
-                edges[i][j] = INF;
+struct Edge
+{
+    int from;
+    int length;
+    Edge(int from,int length):from(from),length(length){}
+};
+
+
+class Graph
+{
+    int** edges;		//邻接矩阵
+    Node* nodes;
+    int n;
+public:
+    Graph(int n) {
+        this->n = n;
+        nodes = new Node[n];
+        for (int i = 0; i < n; ++i) {
+            nodes[i].index = i;
+        }
+
+    }
+    void initEdges() {
+        int a, b, w;
+        while (scanf("%d%d%d", &a, &b, &w) == 3) {
+            nodes[b].addEdge(a,w);
+        }
+    }
+
+    void longest_path_length(int s) {
+        for (int i = 0; i < n; ++i) {
+            nodes[i].dis = -INF;
+        }
+        nodes[s].dis = 0;
+        list<int>& topOrder = this->topsort();
+        for (auto v = topOrder.begin(); v != topOrder.end(); ++v) {
+            if (*v != s) {
+                for(auto e = nodes[*v].edges.begin();e!=nodes[*v].edges.end();++e){
+                    nodes[*v].dis = max(nodes[*v].dis,nodes[e->from].dis+e->length);
+                }
             }
         }
-        dp = new int[n];
-        vis = new int[n];
-        indegree = new int[n];
-        for(int i=0;i<n;++i){dp[i]=-INF;vis[i]=0;indegree[i]=0;v[i].next=NULL;}
-        dp[0]=0;
     }
-    void initEdges(){
-        int a,b,w;
-        while(scanf("%d%d%d",&a,&b,&w)==3){
-            edges[a][b] = w;
-            indegree[b]++;
+
+    void output_path_length(int s) {
+        for (auto p = nodes; p != nodes + n; ++p) {
+            if(p!=nodes+s)
+                printf("%d ",p->dis);
         }
     }
-    int solve(int v){
-        if(vis[v])return dp[v];
-        vis[v]=1;
-        for(int i=0;i<n;i++){
-            if(edges[i][v]!=INF)
-                dp[v] = max(dp[v],solve(i)+edges[i][v]);
+
+    list<int>& topsort() {
+        list<int>* topOrder = new list<int>;
+        for (int i = 0; i < n; ++i) {
+            if (!nodes[i].vis && nodes[i].edges.size() == 0) {
+                visit(i, *topOrder);
+            }
         }
-        return dp[v];
-    }   
-};						
+
+        return *topOrder;
+    }
+private:
+    void visit(int i, list<int>& topOrder) {
+        nodes[i].vis = 1;
+
+        for(auto p = nodes[i].edges.begin();p!=nodes[i].edges.end();++p){
+            if(!nodes[p->from].vis){
+                visit(p->from,topOrder);
+            }
+        }
+
+        topOrder.push_back(i);
+    }
+};
 
 
 
 
 int main()
 {
-    cout<<"I have read the rules about plagiarism punishment"<<endl;
+    //cout << "I have read the rules about plagiarism punishment" << endl;
+    printf("I have read the rules about plagiarism punishment\n");
     int n;
-    cin>>n;
-	MGraph g(n);
+    scanf("%d",&n);
+    Graph g(n);
     g.initEdges();
-    v = (struct node*)malloc(sizeof(struct node)*n);
-	topsort(n,g.indegree);
-	
-    
+    g.longest_path_length(0);
+    g.output_path_length(0);
+    //cout << endl;
+    //system("pause");
     return 0;
 }
 /*
@@ -98,4 +126,5 @@ int main()
 3 4 3
 3 1 4
 4 5 1
+
 */
